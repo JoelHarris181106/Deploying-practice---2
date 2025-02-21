b5a9e18e9f7410183e8055fd4d9b3fd3fb18e81a
joelharris181106 <joel.harris@kalvium.community>
2025-02-21 15:32:38 +0530

    commited

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><h1>joel harris</h1>
+    contact: 1234567890 <br/> 
+    email: xyz@gmail.com
+    </div>
+  )
+}
+
+export default App
